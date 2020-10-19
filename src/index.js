52365b8497800a1629537b22053b282c877d5841
RykeVl <iforox@gmail.com>
2020-10-19 23:05:56 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let nStr = Math.abs(n).toString(),
+        reverseStr = '';
+
+    for (let i = nStr.length - 1; i >= 0; i--) {
+        reverseStr += nStr[i];
+    }
+
+    return reverseStr;  
 }
